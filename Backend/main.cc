0c9ab599052eb69cb4ae2bdb9e67db1e636dd780
Kunal Sawle <ksawle1515@gmail.com>
2026-03-21 02:35:23 +0530

    feat(main): add driver code for scheduling algorithms (RR,SRTF,HRRN,PSNP, and PSP) and stats reporting

--- Backend/main.cc
+++ Backend/main.cc
@@ -6,6 +6,11 @@
 // Tell the compiler about both functions
 void solveFCFS(std::vector<Process> &processes);
 void solveSJF(std::vector<Process> &processes);
+void solveRR(std::vector<Process>& processes, int quantum);
+void solveSRTF(std::vector<Process> &processes);
+void solveHRRN(std::vector<Process> &processes);
+void solvePriorityNonPreemptive(std::vector<Process> &processes, bool isHighPriorityHigher);
+void solvePriorityPreemptive(std::vector<Process> &processes, bool isHighPriorityHigher);
 
 void printTable(const std::vector<Process>& processes, std::string title) {
     std::cout << "\n--- " << title << " ---\n";
@@ -27,22 +32,33 @@ void printTable(const std::vector<Process>& processes, std::string title) {
 void displayStats(const std::vector<Process>& processes, std::string tittle) {
     double totalWait = 0;
     double totalTAT = 0;
+    int totalWorkDone = 0; // Sum of all burst times
     int n = processes.size();
     int maxCompletion = 0;
 
     for (const auto& p : processes) {
         totalWait += p.waitingTime;
         totalTAT += p.turnaroundTime;
-        if (p.completionTime > maxCompletion) maxCompletion =p.completionTime;
+        totalWorkDone += p.burstTime; // Add up all the work the CPU actually did
+        
+        if (p.completionTime > maxCompletion) {
+            maxCompletion = p.completionTime;
+        }
     }
+
+    // Calculation: Total Time minus the time spent working
+    int totalIdleTime = maxCompletion - totalWorkDone;
 
     std::cout << "\n=== Stats for " << tittle << " ===\n";
     std::cout << std::fixed << std::setprecision(2);
-   std::cout << "Average Waiting Time    : " << totalWait / n << " ms\n";
+    std::cout << "Average Waiting Time    : " << totalWait / n << " ms\n";
     std::cout << "Average Turnaround Time : " << totalTAT / n << " ms\n";
-
-    // Throughput = Total Processes / Total Time taken
+    
+    // NEW STATS:
+    std::cout << "Total CPU Idle Time     : " << totalIdleTime << " ms\n";
     if (maxCompletion > 0) {
+        double utilization = ((double)totalWorkDone / maxCompletion) * 100;
+        std::cout << "CPU Utilization         : " << utilization << " %\n";
         std::cout << "Throughput              : " << (double)n / maxCompletion << " processes/ms\n";
     }
     std::cout << "===========================\n";
@@ -50,14 +66,21 @@ void displayStats(const std::vector<Process>& processes, std::string tittle) {
 
 
 int main() {
+
     std::vector<Process> data = {
-    Process(1, 0, 20), // P1: Arrives at 0, takes 20ms (The "Truck")
-    Process(2, 0, 2),  // P2: Arrives at 0, takes 2ms  (The "Bike")
-    Process(3, 0, 1)   // P3: Arrives at 0, takes 1ms  (The "Scooter")
-};
+        Process(1, 2, 10, 1), 
+        Process(2, 2, 2, 5),  
+        Process(3, 4, 4, 3),  
+        Process(4, 6, 1, 4)
+    };
 
     std::vector<Process> fcfsData = data;
     std::vector<Process> sjfData = data;
+    std::vector<Process> rrData = data;
+    std::vector<Process> srtfData = data;
+    std::vector<Process> hrrnData = data;
+    std::vector<Process> psnpData = data;
+    std::vector<Process> pspData = data;
 
     // Run FCFS
     solveFCFS(fcfsData);
@@ -68,6 +91,49 @@ int main() {
     solveSJF(sjfData);
     printTable(sjfData, "SJF Results");
     displayStats(sjfData, "SJF");
+
+     //3. RUN SRTF
+    solveSRTF(srtfData);
+    printTable(srtfData, "SRTF Results");
+    displayStats(srtfData, "SRTF");
+
+     //4. RUN HRRN
+    solveHRRN(hrrnData);
+    printTable(hrrnData, "HRRN Results");
+    displayStats(hrrnData, "HRRN");
+
+    int priorityChoice;
+    std::cout << "\nPriority Mode:\n1. Higher Value = Higher Priority\n2. Lower Value = Higher Priority\nChoice: ";
+    std::cin >> priorityChoice;
+
+    bool isHighHigher = (priorityChoice == 1);
+
+     //5. RUN PSNP
+    solvePriorityNonPreemptive(psnpData, isHighHigher);
+    printTable(psnpData, "PSNP Results");
+    displayStats(psnpData, "PSNP");
+
+     //6. RUN PSP
+    solvePriorityPreemptive(pspData, isHighHigher);
+    printTable(pspData, "PSP Results");
+    displayStats(pspData, "PSP");
+
+    // 7. Run Round Robin 
+    // Dynamic Round Robin Quantum
+    int userQuantum;
+    std::cout << "\nEnter Time Quantum for Round Robin: ";
+    std::cin >> userQuantum;
+
+    if (userQuantum <= 0) {
+        std::cout << "Invalid quantum! Defaulting to 2.\n";
+        userQuantum = 2;
+    }
+    solveRR(rrData, userQuantum);
+    std::string rrTitle = "Round Robin (q=" + std::to_string(userQuantum) + ")";
+    printTable(rrData, "RR Results");
+    displayStats(rrData, "Round Robin  (q=" + std::to_string(userQuantum) + ")");
+
+
 
     return 0;
 }
