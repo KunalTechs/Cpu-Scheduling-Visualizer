686a21ed87369e0a48507ef5dd3bbeba968159e0
Kunal Sawle <ksawle1515@gmail.com>
2026-03-25 16:09:12 +0530

    feat(Backend):add default constructor in process.h

--- Backend/logic/process.h
+++ Backend/logic/process.h
@@ -1,4 +1,4 @@
-#ifndef PROCESS_H  // Header Guard: Prevents double-loading
+#ifndef PROCESS_H  
 #define PROCESS_H
 
 #include <iostream>
@@ -13,10 +13,14 @@ class Process {
     int priority; // for priority algo
 
     // Result fields
-    int completionTime;
-    int waitingTime;
-    int turnaroundTime;
+    int completionTime = 0;
+    int waitingTime = 0;
+    int turnaroundTime = 0;
 
+    // 1. Default Constructor
+    Process() : id(0), arrivalTime(0), burstTime(0), remainingTime(0), priority(0) {}
+
+    // 2. Parameterized Constructor
     Process(int p_id, int at, int bt, int prio = 0){
         id = p_id;
         arrivalTime = at;
