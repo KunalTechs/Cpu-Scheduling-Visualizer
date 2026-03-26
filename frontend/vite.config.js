0608f29ba52d8479ddf5a558b7ebc47035823454
Kunal Sawle <ksawle1515@gmail.com>
2026-03-27 02:00:57 +0530

    feat(Frontend):add react plugin and setup server for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    // hmr: {
+     
+    //   protocol: 'ws',
+    //   host: 'localhost',
+    //   clientPort: 3000, 
+    //},
+  },
 })
