afd085080bc5d20e7479a6f0c8c4d8b52a084e3a
Kunal Sawle <ksawle1515@gmail.com>
2026-04-01 02:33:41 +0530

    feat(Backend):add GanttBlock for GanttChart data from this react Ui make GanttChart

--- Backend/logic/process.h
+++ Backend/logic/process.h
@@ -3,31 +3,35 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+
+// This is what React needs for the Gantt Chart
+struct GanttBlock {
+    std::string id;
+    int start;
+    int end;
+};
 
 class Process {
-    public:
-    int id;
+public:
+    std::string id; 
     int arrivalTime;
     int burstTime;
-    int remainingTime; // Crucial for Round Robin and SRTF
-    int priority; // for priority algo
+    int remainingTime; 
+    int priority; 
 
-    // Result fields
     int completionTime = 0;
     int waitingTime = 0;
     int turnaroundTime = 0;
 
-    // 1. Default Constructor
-    Process() : id(0), arrivalTime(0), burstTime(0), remainingTime(0), priority(0) {}
+    Process() : id("0"), arrivalTime(0), burstTime(0), remainingTime(0), priority(0) {}
 
-    // 2. Parameterized Constructor
-    Process(int p_id, int at, int bt, int prio = 0){
+    Process(std::string p_id, int at, int bt, int prio = 0){
         id = p_id;
         arrivalTime = at;
         burstTime = bt;
         remainingTime = bt;
         priority = prio;
-
     }
 };
 
