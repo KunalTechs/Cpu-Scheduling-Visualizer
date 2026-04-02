f21a8720983c7fde31b1d6857f37663ecfc1d192
Kunal Sawle <ksawle1515@gmail.com>
2026-04-03 02:14:30 +0530

    feat(Frontend):implement Auth page and login and register routes by adding it to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,22 @@ import { Routes, Route } from "react-router-dom"; // Don't import 'Router' here
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-
-
-
+import Auth from "./pages/Auth";
 
 const App = () => {
   return (
     /* No <Router> tag here! It's already in main.jsx */
     <div className="min-h-screen bg-slate-950 text-slate-50 selection:bg-blue-500/30">
-       <Navbar/>
-      
+      <Navbar />
+
       <Routes>
+        {/* PUBLIC */}
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/login" element={<Auth mode="login" />} />
+        <Route path="/register" element={<Auth mode="register" />} />
+
+        {/* SECURE */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
