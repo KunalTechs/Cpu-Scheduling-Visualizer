2d0f2d80a23ab11cc02b7fb97f58bd091e2ad4c7
Kunal Sawle <ksawle1515@gmail.com>
2026-04-05 02:22:04 +0530

    feat(Backend):implement helper function of slecting algo and camparison controller in Simulator.h

--- Backend/controllers/Simulator.h
+++ Backend/controllers/Simulator.h
@@ -11,8 +11,16 @@ public:
   METHOD_LIST_BEGIN
   // 1. Matches React: http://localhost:8081/api/simulate
   ADD_METHOD_TO(Simulator::runSimulation, "/api/simulate", Post, Options, "JwtCookieFilter");
+  ADD_METHOD_TO(Simulator::compareAll, "/api/compare", Post, Options, "JwtCookieFilter");
   METHOD_LIST_END
 
   void runSimulation(const HttpRequestPtr &req,
                      std::function<void(const HttpResponsePtr &)> &&callback);
+                     void compareAll(const HttpRequestPtr &req, 
+                     std::function<void(const HttpResponsePtr &)> &&callback);
+
+private:
+//helper for switch algo compare
+ Json::Value runSpecificAlgo(const std::string &algoName, const Json::Value &processes, int quantum, bool isHighPriorityHigher);
+
 };
