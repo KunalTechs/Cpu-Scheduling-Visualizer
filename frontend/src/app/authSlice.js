7d1cee23ca9e8c3f77beb1c7fd1c1c86fe9b7698
Kunal Sawle <ksawle1515@gmail.com>
2026-04-08 20:31:12 +0530

    feat(Frontend):add authSlice and store for authentication stores and frontend take it fastly

---
+++ frontend/src/app/authSlice.js
@@ -0,0 +1,27 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+        isAuthenticated: false,
+        isCheckingAuth: true,
+    },
+    reducers: {
+        loginSuccess: (state, action) => {
+            state.user =action.payload; // Saves { email, username }
+            state.isAuthenticated =true;
+            state.isCheckingAuth = false;
+        },
+        setAuthChecked: (state) => {
+            state.isCheckingAuth = false; 
+        },
+        logout: (state) => {
+            state.user = null;
+            state.isAuthenticated =false;
+        }, 
+    },
+});
+
+export const { loginSuccess, logout, setAuthChecked } = authSlice.actions;
+export default authSlice.reducer;
