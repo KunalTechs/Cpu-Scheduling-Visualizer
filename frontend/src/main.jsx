152288f808874fce80ec81daea5026d149db428b
Kunal Sawle <ksawle1515@gmail.com>
2026-04-09 20:48:57 +0530

    feat(Frontend):Implement redux persist by adding persist gate in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.jsx'
-import {Provider} from 'react-redux';
-import {store} from './app/store.js'
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from 'react-redux';
+import { store, persistor } from "./app/store";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter> 
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
